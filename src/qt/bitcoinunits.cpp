07d4ef89e10b626957166020b9ac9406acc7f366
lambrosini <luca.ambrosini@supsi.ch>
2021-08-18 10:31:37 +0200

    Change units and labels inside the app

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -44,10 +44,10 @@ QString BitcoinUnits::longName(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("µBTC (bits)");
-    case SAT: return QString("Satoshi (sat)");
+    case BTC: return QString("UZHBTC");
+    case mBTC: return QString("mUZHBTC");
+    case uBTC: return QString::fromUtf8("µUZHBTC (bits)");
+    case SAT: return QString("UZHSatoshi (sat)");
     default: return QString("???");
     }
 }
@@ -66,9 +66,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Bitcoins");
-    case mBTC: return QString("Milli-Bitcoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Bitcoins (bits) (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("UZHBitcoins");
+    case mBTC: return QString("Milli-UZHBitcoins (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-UZHBitcoins (bits) (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     case SAT: return QString("Satoshi (sat) (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
